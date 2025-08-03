c7b57471829dc5a972249c7a2c8185f20aded399
Lakshya Tiwari <imlakshya.tiwari@gmail.com>
2025-08-03 14:22:02 +0530

    almost spotify complete

--- src/getSpotifyPlaylist.js
+++ src/getSpotifyPlaylist.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const axios = require('axios');
 const access_token = process.env.SPOTIFY_ACCESS_TOKEN;
-
+const playListIdMap = new Map(); // map all playlist id -> name 
 async function getSpotifyPlaylist(access_token) {
   let allPlaylists = [];
   let url = 'https://api.spotify.com/v1/me/playlists';
@@ -17,7 +17,6 @@ async function getSpotifyPlaylist(access_token) {
       allPlaylists.push(...response.data.items);
       url = response.data.next; // pagination
     }
-    console.log(allPlaylists);
     return allPlaylists;
   } catch (error) {
     console.error('Error fetching playlists:', error.response?.data || error.message);
@@ -37,4 +36,21 @@ async function getSpotifyPlaylist(access_token) {
 
 // getTracks();
 
-module.exports = getSpotifyPlaylist 
+async function mapping(){
+  const res = await getSpotifyPlaylist(access_token);
+  res.forEach(element => {
+    const id = element.id ;
+    const name = element.name;
+    playListIdMap.set(id,name);
+  })
+}
+
+mapping().then(()=>{
+  console.log("map entries: ");
+  for (let [key,value] of playListIdMap){
+    console.log(key + " " + value);
+  }
+});
+
+
+module.exports = { getSpotifyPlaylist , playListIdMap }
